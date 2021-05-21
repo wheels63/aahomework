c638182e52ab0d963d14e6e7b7986050be82dd1b
tlee <ksltho@gmail.com>
2021-05-21 03:27:14 -0400

    Complete movie_buff hw with solutions advice

--- W5D5/movie_buff/movie_novice/queries.rb
+++ W5D5/movie_buff/movie_novice/queries.rb
@@ -24,18 +24,24 @@
 def find_angelina
   #find Angelina Jolie by name in the actors table
 
+  Actor.find_by(name: 'Angelina Jolie')
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-
+  Movie
+    .select(:id, :title)
+    .where('score >= 9')
+  # Movies.select(where :score >= 9)
 end
 
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
-
+  Movie
+    .select(:id, :title, :yr)
+    .where('title LIKE \'%Star Wars%\'')
 end
 
 
@@ -44,6 +50,11 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
+  Movie
+    .select('yr','COUNT(*) as bad_movies')
+    .where('score < 5')
+    .group('yr')
+    .order('bad_movies DESC')
 
 end
 
@@ -53,14 +64,22 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-
+  Actor
+    .order('name ASC')
+    .limit(10)
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor
+    # .select(:actorsname)
+    # .joins(actors and movie)
+    # .where(movie is pulp fiction)
+    .select(:id, :name)
+    .joins(:movies)
+    .where('title = \'Pulp Fiction\'')
 end
 
 def uma_movies
@@ -68,5 +87,9 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie
+    .select(:id, :title, :yr)
+    .joins(:actors)
+    .where('name = \'Uma Thurman\'')
+    .order('yr ASC')
 end
